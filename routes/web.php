7f8f874adf8703ad776711a9cd23c0d2c93f69f4
DouradoCtrl <gdouradoestudante@gmail.com>
2025-06-30 17:49:46 -0300

    Rotas de formulário

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('formulario', 'form')
-    ->middleware(['auth', 'verified'])
-    ->name('formulario');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
@@ -18,7 +16,8 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 
-    Route::post('formulario/enviar', [ContactController::class, 'send'])->name("formulario.enviar");
+    Route::post('formulario/enviar', [ContactController::class, 'send'])->name('formulario.enviar');
+    Route::get('formulario/contato', [ContactController::class, 'index'])->name('formulario');
 });
 
 require __DIR__.'/auth.php';
