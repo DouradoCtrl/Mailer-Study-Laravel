2a19ccc32b25b2ef8e689aa8fb0ebaa8079c1f9c
DouradoCtrl <gdouradoestudante@gmail.com>
2025-06-30 17:48:51 -0300

    Formulário de contato

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,9 +1,12 @@
 <x-layouts.app :title="__('Principal')">
     <div class="relative h-full flex-1">
-        <flux:heading size="xl" class="mb-5">Envie sua mensagem</flux:heading>
-        <flux:input type="email" name="nome" class="mb-3" label="Nome" />
-        <flux:textarea name="message" label="Sua mensagem" />
+        <form action="{{ route('formulario.enviar') }}" method="POST">
+            @csrf
+            <flux:heading size="xl" class="mb-5">Envie sua mensagem</flux:heading>
+            <flux:input type="text" name="nome" class="mb-3" label="Nome" />
+            <flux:textarea name="message" label="Sua mensagem" />
 
-        <flux:button variant="primary" class="w-full mt-5">Enviar mensagem</flux:button>
+            <flux:button variant="primary" class="w-full mt-5" type="submit">Enviar mensagem</flux:button>
+        </form>
     </div>
 </x-layouts.app>
