2ecab195e3da91bd0689d09a4321c3f676ce62c9
DouradoCtrl <gdouradoestudante@gmail.com>
2025-07-01 17:07:52 -0300

    Criação do formulário para envio de mensagem

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -3,8 +3,17 @@
         <form action="{{ route('formulario.enviar') }}" method="POST">
             @csrf
             <flux:heading size="xl" class="mb-5">Envie sua mensagem</flux:heading>
-            <flux:input type="text" name="nome" class="mb-3" label="Nome" />
-            <flux:textarea name="message" label="Sua mensagem" />
+            <flux:select wire:model="industry" name="subject" placeholder="Assunto" class="mb-3">
+                <flux:select.option>Dúvidas</flux:select.option>
+                <flux:select.option>Suporte</flux:select.option>
+                <flux:select.option>Atendimento</flux:select.option>
+                <flux:select.option>Opinião</flux:select.option>
+                <flux:select.option>Elogio</flux:select.option>
+                <flux:select.option>Outros</flux:select.option>
+            </flux:select>
+{{--            <flux:input type="text" name="name" class="mb-3" label="Nome" />--}}
+{{--            <flux:input type="hidden" name="email" class="mb-3" value="teste@gmail.com"/>--}}
+            <flux:textarea name="message" label="Sua mensagem"/>
 
             <flux:button variant="primary" class="w-full mt-5" type="submit">Enviar mensagem</flux:button>
         </form>
