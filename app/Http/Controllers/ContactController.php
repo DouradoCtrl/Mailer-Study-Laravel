7cebf91e395c711a516679842a02335f9f155eaf
DouradoCtrl <gdouradoestudante@gmail.com>
2025-07-01 17:05:09 -0300

    criação do send aplicando ToMail para envio de email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\Contact;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class ContactController extends Controller
 {
@@ -10,7 +13,17 @@ class ContactController extends Controller
         return view('form');
     }
 
-    public function send() {
+    public function send(Request $request) {
+        $id = auth()->id();
+        $user = auth()->loginUsingId($id);
 
+        $nome = Str::before($user->name, ' ');
+
+        $sent = Mail::to("contato@kayroslink.com", "Kayros")->send(new Contact([
+            'fromName' => $nome,
+            'fromEmail' => $user->email,
+            'message' => $request->message,
+            'subject' => $request->subject,
+        ]));
     }
 }
